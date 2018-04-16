e46a0ecdac4b1847c6653638c5c55ca3b4601fcb
xtati <xtati@DESKTOP-ATSTFNO>
2018-04-16 14:10:22 -0500

    commit inicial

---
+++ StockMarketApp/stockMarket-WebPortal/src/main/java/mx/gob/sat/mat/tabacos/vista/UserLoginView.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.gob.sat.mat.tabacos.vista;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.primefaces.PrimeFaces;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author xtati
+ */
+@Controller("userLoginView")
+@Scope(value = "view")
+public class UserLoginView {
+    @PostConstruct
+    private void init(){
+        System.err.println("se construye");
+    }
+
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void login(ActionEvent event) {
+        FacesMessage message = null;
+        boolean loggedIn = false;
+
+        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
+            loggedIn = true;
+            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+        } else {
+            loggedIn = false;
+            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
+        }
+
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
+    }
+}
