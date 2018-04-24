1efd39f27c2ecf42037cd2efe1a192dc6fb679fc
Juan <Juan@Juan-PC>
2018-04-24 12:47:01 -0500

    Se cambia el tema a ui-lightness y se agregan validaciones en dto´s

--- StockMarketApp/stockMarket-DTO/src/main/java/stock/horizontal/dto/FondoEmisora.java
+++ StockMarketApp/stockMarket-DTO/src/main/java/stock/horizontal/dto/FondoEmisora.java
@@ -6,6 +6,9 @@
 package stock.horizontal.dto;
 
 import java.io.Serializable;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 /**
  *
@@ -14,13 +17,13 @@ import java.io.Serializable;
 public class FondoEmisora implements Serializable{
     
     private String emisora;
-//    @Digits(integer = 3, fraction = 0)
-//    @Min(value = 001)
-//    @Max(value = 999)
+    @Digits(integer = 3, fraction = 0)
+    @Min(value = 001)
+    @Max(value = 999)
     private Integer acciones;
-//    @Digits(integer = 3, fraction = 2)
-//    @Min(value = 01)
-//    @Max(value = 999)
+    @Digits(integer = 3, fraction = 2)
+    @Min(value = 01)
+    @Max(value = 999)
     private Double importe;
 
     public String getEmisora() {
